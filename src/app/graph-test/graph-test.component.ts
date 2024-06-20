a2a6c4aa7676fe0799dfd399d8307924891a1ed6
jeremy_felix <jeremy.felix@scantech.com>
2024-06-20 17:15:03 +0200

    3d echarts graph displayed with real datas

--- src/app/graph-test/graph-test.component.ts
+++ src/app/graph-test/graph-test.component.ts
@@ -1,7 +1,7 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
 import { NgxEchartsModule } from 'ngx-echarts';
-//import { EChartsOption } from 'echarts';
+// import { EChartsOption } from 'echarts';
 import { CommonModule } from '@angular/common';
 // import { ECharts } from 'echarts';
 // import { ECharts } from 'echarts/core';
@@ -9,6 +9,39 @@ import 'echarts-gl';
 // import * as echarts from 'echarts';
 // import { graphic } from 'echarts';
 import data from './data.json';
+
+function generateDemoData() {
+  var demoData = [];
+  for (var i = 0; i <= 50; i++) {
+    for (var j = 50; j > 0; j--) {
+      demoData.push([i, j, i + j]);
+    }
+  }
+  console.log("demoData=");
+  console.log(demoData);
+  return demoData;
+}
+
+function transformRealDataStructure(data:any[], minXValue:number, maxXValue:number) {
+
+  var dataTransformed:any[] = [];
+
+  data.forEach((profil:any, index:number) => { // je parcours mes tableaux de profils...
+
+      console.log("profil "+index+" =");
+      console.log(profil);
+
+      profil.forEach((points:any) => { // je parcours mes tableaux de points (x, y et z)...
+       if((points[0] > minXValue) && (points[0] < maxXValue)){ // Cette condition permet d'enlever les rebords latéraux du graph 3d
+          dataTransformed.push(points);
+       }
+      })
+  });
+
+  console.log("dataTransformed=");
+  console.log(dataTransformed);
+  return dataTransformed;
+}
 
 
 @Component({
@@ -22,23 +55,7 @@ import data from './data.json';
   ]
 })
 
-export class GraphTestComponent implements OnInit {
-
-  /*
-  constructor(){
-    
-    this.measureDatas = data;
-    
-    console.log('data=', data);
-    //console.log('measureDatas=', this.measureDatas);
-  }
-  */
-
-  ngOnInit(): void {
-    this.measureDatas = data;
-    console.log('data2=', data);
-
-  }
+export class GraphTestComponent /*implements OnInit*/ {
 
   @NgModule({
     imports: [
@@ -47,306 +64,99 @@ export class GraphTestComponent implements OnInit {
          * This will import all modules from echarts.
          * If you only need custom modules,
          * please refer to [Custom Build] section.
-        */
-       echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
+         */
+        echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
       }),
     ],
   })
+
+  ngOnInit(): void {
+    console.log("data=");
+    console.log(data);
+  }
+
   
-
-
-  // Cette fonction permet, pour chaque profil, de parcourir chaque valeur soit de x, soit de y, soit de z dans tous le tableau de données. OK
-  selectAxisValuesForAllProfils(axis: "x" | "y" | "z", data:any[]) {
-    //let data = this.measureDatas;
-    let axisValues:any[] = [];
-
-    console.log("measureDatas="); // OK, le tableau est complet !
-    console.log(data); // Le résultat s'affiche en violet dans la console (au début)
-
-    for (let m = 0; m < data.length; m++) { // pour chaque profil...
-
-      for (let n = 0; n < data.length; n++) { // pour chaque ensemble de valeurs x, y et z...
-
-        if(axis === "x"){
-          axisValues.push(data[m][n][0]);
-          console.log("data[m][n][0]=" + data[m][n][0])
-        } else if(axis === "y"){
-          axisValues.push(data[m][n][1]);
-          console.log("data[m][n][1]=" + data[m][n][1])
-
-        } else if(axis === "z"){
-          axisValues.push(data[m][n][2]);
-          console.log("data[m][n][2]=" + data[m][n][2])
-
-        }      
-      }
-    }
-
-    console.log(`axisValues for axis "${axis}" de longueur ${axisValues.length}`) // Il y a une longueur de 784 éléments pour chacun des axes.
-    console.log(axisValues);
-
-    return axisValues;
-  };
-
-
-
+  // Copié-collé de : https://www.npmjs.com/package/ngx-echarts?activeTab=readme
   title = 'echarts_playground';
-  measureDatas:any[] = data; // Où la valeur data est-elle assignée ?
 
-  /*
-  profilIndex = 0;
-  valueIndex = 0;
-  */
+  minXValue = 25;
+  maxXValue = 2275;
 
-  xValues = this.selectAxisValuesForAllProfils("x", this.measureDatas);
-  yValues = this.selectAxisValuesForAllProfils("y", this.measureDatas);
-  zValues = this.selectAxisValuesForAllProfils("z", this.measureDatas);
+  demoData = generateDemoData();
+  realDataTransformed = transformRealDataStructure(data, this.minXValue, this.maxXValue);
 
 
   chartOption: /*EChartsCoreOption*/ /*echarts.EChartsCoreOption*/ any  = {
     
-    // Démo copiée-collée de : https://www.npmjs.com/package/ngx-echarts?activeTab=readme
-    /*
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-      },
-    ],
-    */
-   
-
-    
-    tooltip: {},
-    legend: {},
     visualMap: {
-      max: 2,
+      show: false,
+      dimension: 2,
+      //min: 0,
+      //max: 4,
       inRange: {
-        color: ['#d94e5d', '#eac736', '#50a3ba']
+          color: ['#313695', '#e0f3f8', '#f46d43']
       }
     },
+
     xAxis3D: {
-      //show: true,
-      //data: [-0.4, 0.25, -0.8],
-      //data: "xAxis3D",
       type: 'value',
-      data: this.xValues
+      min: this.minXValue, // Permet de déterminer à partir de quand commencer à afficher l'échelle des valeurs sur l'axe X
+      max: this.maxXValue // Permet de déterminer à partir de quand finir d'afficher l'échelle des valeurs sur l'axe X
     },
+
     yAxis3D: {
-      //show: true,
-      //data: [-0.8, 0.5, -0.7],
-      //data: "yAxis3D",
       type: 'value',
-      data: this.yValues
+      min: data[0][1],
+      max: data[data.length-1][1]
     },
+
     zAxis3D: {
-      //show: true,
-      //data: [-0.6, 0.7, -0.5],
-      //data: "zAxis3D",
       type: 'value',
-      data: this.zValues
+      min: 2250,
+      max: 2350
     },
+
     grid3D: {
+      axisLine: {
+        lineStyle: { color: '#fff' }
+      },
+      axisPointer: {
+        lineStyle: { color: '#fff' }
+      },
       viewControl: {
-        projection: 'perspective'
-      }
-    },
-    
-
-    // Copié-collé de la démo du tuto suivant : https://echarts.apache.org/handbook/en/concepts/dataset/
-    /*
-    xAxis: {
-      type: 'category',
-      data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie']
-    },
-    yAxis: {},
-    */
-
-
-    series: [
-
-      /*
-      // Copié-collé de la démo du tuto suivant : https://echarts.apache.org/handbook/en/concepts/dataset/
-      {
-        type: 'bar',
-        name: '2015',
-        data: [89.3, 92.1, 94.4, 85.4]
+        // autoRotate: true
       },
-      {
-        type: 'bar',
-        name: '2016',
-        data: [95.8, 89.4, 91.2, 76.9]
-      },
-      {
-        type: 'bar',
-        name: '2017',
-        data: [97.7, 83.1, 92.5, 78.1]
-      }
-      */
-
-      
-
-      /*
-      {
-        type: 'surface',
-        wireframe: {
-          show: false // Gestion de l'affichage des quadriages
-        },
-
-        symbolSize: 50,
-                
-        itemStyle: {
-          opacity: 1 // Gère la transparence du graph
-        },
-      },
-      */
-      
-
-      
-      {
-        type: 'surface',
-        //name: 'x',
-        data: this.xValues,  // [1, 0, 2, 4], // Attribut obligatoire pour que le graph s'affiche
-        min: 0,
-        max: 30
-      },
-
-      {
-        type: 'surface',
-        //name: 'y',
-        data: this.yValues, // [2, 1, 3, 1], // Attribut obligatoire pour que le graph 
-        min: 0,
-        max: 2000000000000
-      },
-
-      {
-        type: 'surface',
-        //name: 'z',
-        data: this.zValues, // [2, 4, 2, 0], // Attribut obligatoire pour que le graph 
-        min: 0,
-        max: 3000
-      },
-      
-
-      /*
-      equation: {
-        x: {
-          //step: 0.05,
-          //min: -1,
-          //max: 1,
-          values: this.xValues,
-
-          data: function() { // CETTE FONCTION N'EST JAMAIS EXECUTEE !
-            for (let i = 0; i < this.values.length; i++) {
-              console.log("X > this.values=" + this.values);
-              return this.values[i];
-            }
-          }
-        },
-        y: {
-          //step: 0.05,
-          //min: -1,
-          //max: 1,
-          values: this.yValues,
-
-          data: function() { // CETTE FONCTION N'EST JAMAIS EXECUTEE !
-            for (let i = 0; i < this.values.length; i++) {
-              console.log("Y > this.values=" + this.values);
-              return this.values[i];
-            }
-          }
-        },
-        z: {
-          //step: 0.05,
-          //min: -1,
-          //max: 1,
-          values: this.zValues,
-
-          data: function() { // CETTE FONCTION N'EST JAMAIS EXECUTEE !
-            for (let i = 0; i < this.values.length; i++) {
-              console.log("Z > this.values=" + this.values);
-              return this.values[i];
-            }
-          }
-          
-          function (x:number, y:number) {
-            if (Math.abs(x) < 0.1 && Math.abs(y) < 0.1) {
-                return '-';
-            }
-            return Math.sin(x * Math.PI) * Math.sin(y * Math.PI);
-          }
-          
+      light: {
+        main: {
+          shadow: true,
+          quality: 'ultra',
+          intensity: 1.5
         }
       }
-      */
-     
-    ]
-    
-      
-    /*
+    },
+
     series: [
       {
-        //data: [820, 932, 901, 934, 1290, 1330, 1320]
-        //data: [{x:1, y:1}, {x:2, y:2}, {x:3, y:3}],
-
-        data: [],
+        //type: 'scatter3D',
+        //type: 'bar3D',
         type: 'surface',
+
+        /*
         wireframe: {
-          // show: false
+          show: false
         },
-        // equation: {
-        //   x: {
-        //     step: 0.05
-        //   },
-        //   y: {
-        //     step: 0.05
-        //   },
-        //   z: function (x:number, y:number) {
-        //     if (Math.abs(x) < 0.1 && Math.abs(y) < 0.1) {
-        //       return '-';
-        //     }
-        //     console.log("FONCTION DU Z")
-        //     return Math.sin(x * Math.PI) * Math.sin(y * Math.PI);
-        //   }
-        // }
+        */
+        
+        //data: this.demoData,
+        data: this.realDataTransformed,
+
+        shading: 'lambert',
+        label: {
+          formatter: function (param:any) {
+            return param.value[2].toFixed(1);
+          }
+        }
       }
     ]
-    */
-   
   }
-
-
-  /*
-  // Cette fonction permet, pour chaque profil, de parcourir chaque valeur soit de x, soit de y, soit de z dans tous le tableau de données.
-  assignProfilValues(axis:string) {
-    let data = this.measureDatas;
-
-    console.log("measureDatas=" + data);
-
-    //let profilLength = data.length;
-    //let profilValuesLength = data[0].length;
-
-
-    for (let m = 0; m < data.length; m++) { // pour chaque profil...
-
-      for (let n = 0; n < data.length; n++) { // pour chaque ensemble de valeurs x, y et z...
-
-        if(axis === "x"){
-          this.chartOption.series.equation.x = data[m][n][0];
-        } else if(axis === "y"){
-          this.chartOption.series.equation.y = data[m][n][1];
-        } else if(axis === "z"){
-          this.chartOption.series.equation.z = data[m][n][2];
-        }      
-      }
-    }
-  }
-  */
 }
